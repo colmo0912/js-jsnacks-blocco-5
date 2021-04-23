eae354d08b162c09ea577801981a61f1e722f080
colmo0912 <colmo0912@gmail.com>
2021-04-23 16:16:40 +0200

    primo esercizio finito

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-var pesiZucchine = [];
-var varietàZucchine = [
+// esercizio 1
 
-    zucchina1= {
+var varietaZucchine = [
+
+    zucchina1 = {
 
         varieta: "varieta1",
-        peso : 200,
+        peso: 200,
         lunghezza : 15 
     },
 
-    zucchina2= {
+    zucchina2 = {
 
         varieta: "varieta2",
-        peso : 200,
-        lunghezza : 15 
+        peso: 200,
+        lunghezza: 15 
     },
 
     zucchina3 = {
 
         varieta: "varieta3",
-        peso : 200,
-        lunghezza : 15 
+        peso: 200,
+        lunghezza: 15 
     },
 
     zucchina4 = {
 
         varieta: "varieta4",
-        peso : 300,
-        lunghezza : 15 
+        peso: 300,
+        lunghezza: 15 
     },
 
     zucchina5 = {
 
         varieta: "varieta5",
-        peso : 200,
-        lunghezza : 15 
+        peso: 200,
+        lunghezza: 15 
     },
 
     zucchina6 = {
@@ -53,42 +54,39 @@ var varietàZucchine = [
     zucchina8 = {
 
         varieta: "varieta8",
-        peso : 200,
-        lunghezza : 15 
+        peso: 200,
+        lunghezza: 15 
     },
 
     zucchina9 = {
 
         varieta: "varieta9",
-        peso : 200,
-        lunghezza : 15 
+        peso: 200,
+        lunghezza: 15 
     },
 
     zucchina10 = {
 
         varieta: "varieta10",
-        peso : 400,
-        lunghezza : 15 
+        peso: 400,
+        lunghezza: 15 
     }
 
 ]
 
 
-console.log (varietàZucchine)
+// console.log (varietàZucchine)
+var pesoTotaleZucchine=0;
 
-for (var i=0; i<varietàZucchine.length; i++){
+for (var i=0; i<varietaZucchine.length; i++){
 
-    var numeroZucchine = varietàZucchine[i]
-    // console.log(numeroZucchine);
-    for (var key in varietàZucchine[i]){
+    var pesoSingolaZucchina = varietaZucchine[i].peso;
+        console.log(pesoSingolaZucchina);
+        
+        pesoTotaleZucchine+=pesoSingolaZucchina;
     
-        // var pesoZucchinaSingola = console.log(numeroZucchine.peso)
-        // pesiZucchine.push(pesoZucchinaSingola)
-        var pesoZucchinaSingolo = console.log(varietàZucchine[i].peso)
-        console.log(pesiZucchine.push(pesoZucchinaSingolo))
-    }
-
 }
+    console.log(pesoTotaleZucchine)
 
 
 
@@ -96,12 +94,13 @@ for (var i=0; i<varietàZucchine.length; i++){
 
 
 
-// for (var j = 0; j<numeroZucchine.length; j++){
 
-//     var pesoZucchine = numeroZucchine.peso[j]
-//     pesoZucchine.push(pesoTotaleZucchine)
-    
-// }
+
+
+
+
+
+
 
 
 
